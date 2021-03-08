34f3e3d0119a10be20369d190c9a1007f7092e4c
Sujith <sujithramanathan@gmail.com>
2021-03-08 21:33:35 +0530

    Added logback.xml

--- src/main/java/com/gds/api/service/impl/RFServiceImpl.java
+++ src/main/java/com/gds/api/service/impl/RFServiceImpl.java
@@ -13,7 +13,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
